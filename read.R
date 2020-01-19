59b4e55c6785010e054c75fddcb87e97b0c45264
sapowers <sdp7xc@virginia.edu>
2020-01-19 12:56:04 +0200

    Adding To Exploration

--- read.R
+++ read.R
@@ -50,8 +50,8 @@ names2[69:76] <- paste0(rep("ProsReleaseConditions",8), ".", names2[69:76])
 names2[77:83] <- paste0(rep("ProsBondArg",7), ".", names2[77:83])
 names2[87:94] <- paste0(rep("CourtSetReleaseConditions",8), ".", names2[87:94])
 names2[95] <- "BondReturnDate"
-names2[86] <- "OtherComments"
-names2[86] <- "SuggestionsforTwitter"
+names2[96] <- "OtherComments"
+names2[97] <- "SuggestionsforTwitter"
 
 # use new clean names for data
 names(cw) <- names2
@@ -86,8 +86,8 @@ cw$CaseResolved <- ifelse(cw$CaseResolved=="Yes", 1, 0)
 
 # relevel factors
 cw <- mutate(cw, 
-             DefGender=fct_relevel(DefGender, "Man"), 
-             DefRace=fct_relevel(DefRace, "White"))
+             DefGender=fct_relevel(DefGender, "Man"), # Makes Man Base factor for analysis in Gender
+             DefRace=fct_relevel(DefRace, "White"))   # White is base factor for analysis in Race
 
 # create binary indicators for cash bail requests and sets
 cw$CashBailSet <- ifelse(cw$Bondsetbycourt %in% c("Cash or Surety (C/S)", "Cash Only"), 1, 0)
@@ -112,6 +112,45 @@ table(cw$DefRepByCounsel) # this is important
 ggplot(cw, aes(WhichCourt, fill=DefRepByCounsel)) + geom_bar(position="fill")
 ggplot(cw, aes(DefRace, fill=DefRepByCounsel)) + geom_bar(position="fill") + facet_wrap(~WhichCourt, scales="free_y")
 ggplot(cw, aes(DefRace, fill=DefRepByCounsel)) + geom_bar(position="dodge") + facet_wrap(~WhichCourt, scales="free_y")
+
+ggplot(cw, aes(DefRace, fill=DefRepByCounsel)) + geom_bar(position="dodge") + facet_wrap(~WhichCourt, scales="free_y")
+
+
+cw %>%  
+  group_by( WhichCourt, DefRace) %>%
+  add_tally() %>%
+  group_by( WhichCourt, DefRace, n, DefRepByCounsel) %>%
+  summarize(count= n()) %>%
+  ungroup() %>%
+  mutate(percent = count/n*100) %>%
+  spread(DefRepByCounsel, percent, fill = 0) %>%
+  select(-`<NA>`) %>%
+  group_by(WhichCourt, DefRace, n) %>%
+  summarise(percent = sum(`1`)) %>%
+  filter(is.na(WhichCourt) == FALSE) %>%
+  
+  ggplot( aes(x = reorder(WhichCourt, percent), y = percent, fill = reorder(DefRace, percent))) + 
+  geom_bar(stat = "identity", position = "dodge", alpha = .8, color = "black") +
+  
+  ggtitle('Percent of Defendents Receiving Representation by Race & Court') +
+  theme(
+    plot.title = element_text( face="bold", hjust = 0.5 ),
+    legend.title = element_text(),
+    axis.title.x = element_text(vjust=-2),
+    axis.title.y = element_text(vjust= 2),
+    axis.text.x=element_text(vjust=-3),
+    axis.ticks.x = element_blank()
+    
+  )+ 
+  scale_y_continuous(labels = function(x) paste0(round(x), "%"), expand = c(0, 0 ), limits = c(-1, 110)) +
+  geom_text(aes(label= paste0(round(percent), "%"), y = percent+1), position = position_dodge(width = .9), size = 2  ) +
+  geom_text(aes(label= paste0( "n",n), y = 0), position = position_dodge(width = .9), vjust = 3, size = 2 ) +
+  labs(x="Court", y="Defendants Represented, %", fill = "Race")  +
+  coord_cartesian(clip = 'off')+
+  scale_fill_brewer(palette="Set1")
+
+
+
 
 cw$Charges # this needs to be extensively cleaned
 
@@ -163,6 +202,20 @@ summary(mod)
 mod <- glm(CashBailSet ~ DefRace + DefGender, family="binomial", data=cw)
 summary(mod)
 
+modbailset <- as.data.frame( summary(mod)$coef)
+modbailset$variable <- row.names(mod1)
+modbailset1 <-
+  modbailset %>%
+  mutate(OR = round(exp(Estimate),2),
+         lower = round(exp(Estimate - (1.96*`Std. Error`)),2),
+         upper = round(exp(Estimate + (1.96*`Std. Error`)),2),
+         measure = paste0(OR, " (",lower,"-",upper,")"),
+         p_value =round(`Pr(>|z|)`,3),
+         p_value = ifelse(p_value == 0, "<0.001", p_value)) %>%
+  select(variable,  measure, p_value )
+
+modbailset1
+
 cor(cw$CashBailSet, cw$ProsCashBail)
 xtabs(cw$ProsCashBail ~ cw$CashBailSet)
 
@@ -170,4 +223,5 @@ xtabs(cw$ProsCashBail ~ cw$CashBailSet)
 # ideas: 
 # calculate average time from start to end? by court?
 
+write.csv(cw, file = "courtwatch_clean.csv")
 
